50cb7c68ba54f3ee14e71298d93dcc26be794b9a
Maxime Biloé <maxime.biloe@gmail.com>
2017-02-01 10:31:18 +0100

    // Version updated to v2.0.0

--- ps_contactinfo.php
+++ ps_contactinfo.php
@@ -42,7 +42,7 @@ class Ps_Contactinfo extends Module implements WidgetInterface
     {
         $this->name = 'ps_contactinfo';
         $this->author = 'PrestaShop';
-        $this->version = '1.0.5';
+        $this->version = '2.0.0';
 
         $this->bootstrap = true;
         parent::__construct();
