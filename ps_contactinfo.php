9b5f2d0df27e5a23ee6e7d6d2468546e18e6dd49
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-01 17:39:55 +0100

    // Version updated to v2.0.1

--- ps_contactinfo.php
+++ ps_contactinfo.php
@@ -42,7 +42,7 @@ class Ps_Contactinfo extends Module implements WidgetInterface
     {
         $this->name = 'ps_contactinfo';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
 
         $this->bootstrap = true;
         parent::__construct();
